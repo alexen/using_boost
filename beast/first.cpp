a6b96cd963fc4e953e3afee447e401385d73e703
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2019-10-24 15:11:00 +0300

    Реализована отправка SOAP-запроса в тестовый контур ФСС через HTTPS

--- beast/first.cpp
+++ beast/first.cpp
@@ -7,14 +7,29 @@
 #include <iostream>
 
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 #include <boost/beast.hpp>
-#include <boost/beast/http/buffer_body.hpp>
+
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/iostreams/copy.hpp>
 #include <boost/core/ignore_unused.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 
 
-using tcp = boost::asio::ip::tcp;
+namespace ip = boost::asio::ip;
+using tcp = ip::tcp;
 namespace http = boost::beast::http;
+
+
+//POST https://docs-test.fss.ru/ws-insurer-v11/FileOperationsLnPort HTTP/1.1
+//Accept-Encoding: gzip,deflate
+//Content-Type: text/xml;charset=UTF-8
+//SOAPAction: "http://ru/ibs/fss/ln/ws/FileOperationsLn.wsdl/getPrivateLNData"
+//Content-Length: 15358
+//Host: docs-test.fss.ru
+//Connection: Keep-Alive
+//User-Agent: Apache-HttpClient/4.1.1 (java 1.5)
 
 
 int main( int argc, char** argv )
@@ -22,21 +37,61 @@ int main( int argc, char** argv )
      boost::ignore_unused( argc, argv );
      try
      {
-          static constexpr auto path = "/";
-          static constexpr auto httpVersion = 10;
+          static constexpr auto host = "docs-test.fss.ru";
+          static constexpr auto protocol = "https";
+          static constexpr auto path = "/ws-insurer-v11/FileOperationsLnPort";
+          static constexpr auto httpVersion = 10u;
+
+          static const boost::filesystem::path soapBodyPath =
+               argc > 1 ? argv[ 1 ] : "./soap_body.xml";
+          static constexpr auto soapAction =
+               "http://ru/ibs/fss/ln/ws/FileOperationsLn.wsdl/getPrivateLNData";
+          static constexpr auto contentType = "text/xml;charset=UTF-8";
 
           boost::asio::io_context ioctx;
 
+          boost::asio::ssl::context sslctx{ boost::asio::ssl::context::sslv23_client };
+
           tcp::resolver resolver{ ioctx };
-          tcp::socket socket{ ioctx };
+          boost::asio::ssl::stream< tcp::socket > stream{ ioctx, sslctx };
 
-          boost::asio::connect( socket, resolver.resolve( "localhost", "9999" ) );
+          boost::asio::connect( stream.next_layer(), resolver.resolve( host, protocol ) );
 
-          http::request< http::string_body > request{ http::verb::get, path, httpVersion };
-          request.set( http::field::host, "localhost" );
+          stream.handshake( boost::asio::ssl::stream_base::client );
+
+          boost::system::error_code error;
+          http::file_body::value_type body;
+          body.open( soapBodyPath.c_str(), boost::beast::file_mode::read, error );
+          if( error )
+          {
+               BOOST_THROW_EXCEPTION( boost::system::system_error{ error } );
+          }
+
+          http::request< http::file_body > request{ http::verb::post, path, httpVersion };
+          request.set( http::field::host, host );
           request.set( http::field::user_agent, __PRETTY_FUNCTION__ );
+          request.set( http::field::content_length, body.size() );
+          request.set( http::field::soapaction, soapAction );
+          request.set( http::field::content_type, contentType );
+          request.body() = std::move( body );
 
-          http::write( socket, request );
+          http::write( stream, request );
+
+          boost::beast::flat_buffer buffer;
+          http::response< http::string_body > response;
+
+          http::read( stream, buffer, response );
+
+          if( response.result() == boost::beast::http::status::ok )
+          {
+               if( response.body().empty() )
+               {
+                    BOOST_THROW_EXCEPTION( std::runtime_error{ "empty response received" } );
+               }
+               std::cout << response.body();
+          }
+
+          stream.shutdown();
      }
      catch( const std::exception& e )
      {
