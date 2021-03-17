0bebf65bf60cb480231fdc8e9b41858043ac8637
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-03-17 15:21:18 +0300

    Regex: Изменен принцип работы noexcept-функции - использован std::exception_ptr

--- regex/main.cpp
+++ regex/main.cpp
@@ -13,27 +13,41 @@
 #include <boost/utility/string_view.hpp>
 
 
-using OnError = std::function< void( void ) >;
+std::tuple< std::string, std::string > parseFormalizedFileName( const std::string& fileName );
+std::tuple< std::string, std::string > parseFormalizedFileName( const std::string& fileName, std::exception_ptr& eptr ) noexcept;
 
-std::tuple< std::string, std::string > parseFormalizedFileName( const std::string& fileName, OnError&& raiseError )
-{
-     static constexpr auto expectedMatches = 2u;
-     static const boost::regex re{ R"re(^(\d{3}-?\d{3}-?\d{6})_(\d{3}-?\d{3})_[0-9a-f]{32}\.zip$)re", boost::regex::icase };
-     boost::smatch matches;
-     if( boost::regex_search( fileName, matches, re ) && matches.size() == expectedMatches + 1 )
-     {
-          return std::make_tuple( matches[ 1 ], matches[ 2 ] );
-     }
-     raiseError();
-     return {};
-}
 
 
 std::tuple< std::string, std::string > parseFormalizedFileName( const std::string& fileName )
 {
-     return parseFormalizedFileName( fileName,
-          [ &fileName ]{ BOOST_THROW_EXCEPTION( std::invalid_argument{ "invalid transport package name format: " + fileName } ); } );
+     static constexpr auto expectedMatches = 2u;
+     static const boost::regex re{ R"re(^(\d{3}-?\d{3}-?\d{6})_(\d{3}-?\d{3})_[0-9a-f]{32}\.zip$)re", boost::regex::icase };
+     boost::smatch matches;
+     if( !boost::regex_search( fileName, matches, re ) && matches.size() == expectedMatches + 1 )
+     {
+          BOOST_THROW_EXCEPTION( std::invalid_argument{ "invalid file name format: " + fileName } );
+     }
+     return std::make_tuple( matches[ 1 ], matches[ 2 ] );
 }
+
+
+std::tuple< std::string, std::string > parseFormalizedFileName( const std::string& fileName, std::exception_ptr& eptr ) noexcept
+{
+     try
+     {
+          return parseFormalizedFileName( fileName );
+     }
+     catch( ... )
+     {
+          eptr = std::current_exception();
+     }
+     return {};
+}
+
+
+#define PRINT_SORRY_IF( condition_, message_ ) \
+     do { if( (condition_) ){ \
+          std::cout << "sorry, some error occured: " << message_ << '\n'; } } while( false )
 
 
 int main( int argc, char** argv )
@@ -41,15 +55,39 @@ int main( int argc, char** argv )
      boost::ignore_unused( argc, argv );
      try
      {
-          std::string rns, num;
-          std::tie( rns, num ) = parseFormalizedFileName( "001-001-163500_001-001_218c384544b4489b9fb08c84bdc53183.ZIP" );
-          std::cout
-               << "rns: " << rns << '\n'
-               << "num: " << num << '\n';
-          std::tie( rns, num ) = parseFormalizedFileName( "BAD_PACKAGE_NAME.EXT" );
-          std::cout
-               << "rns: " << rns << '\n'
-               << "num: " << num << '\n';
+          try
+          {
+               std::string rns, num;
+               std::tie( rns, num ) = parseFormalizedFileName( "001-001-163500_001-001_218c384544b4489b9fb08c84bdc53183.ZIP" );
+               std::cout
+                    << "rns: " << rns << '\n'
+                    << "num: " << num << '\n';
+
+               std::tie( rns, num ) = parseFormalizedFileName( "BAD_PACKAGE_NAME.EXT" );
+               std::cout
+                    << "rns: " << rns << '\n'
+                    << "num: " << num << '\n';
+          }
+          catch( const std::exception& e )
+          {
+               std::cout << "parsing file name error: " << e.what() << '\n';
+          }
+
+          std::exception_ptr eptr;
+          {
+               std::string rns, num;
+               std::tie( rns, num ) = parseFormalizedFileName( "001-001-163500_001-001_218c384544b4489b9fb08c84bdc53183.ZIP", eptr );
+               std::cout
+                    << "rns: " << rns << '\n'
+                    << "num: " << num << '\n';
+               PRINT_SORRY_IF( eptr, "cannot parse file name" );
+
+               std::tie( rns, num ) = parseFormalizedFileName( "BAD_PACKAGE_NAME.EXT", eptr );
+               std::cout
+                    << "rns: " << rns << '\n'
+                    << "num: " << num << '\n';
+               PRINT_SORRY_IF( eptr, "cannot parse file name" );
+          }
      }
      catch( const std::exception& e )
      {
