0858b50527185189da0b57b61ae4f8a15fe68dc5
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-04-01 10:50:36 +0300

    Variant: Добавлена работа с boost::variant

---
+++ variant/main.cpp
@@ -0,0 +1,69 @@
+/// @file
+/// @brief
+/// @copyright Copyright (c) InfoTeCS. All Rights Reserved.
+
+#include <stdexcept>
+#include <iostream>
+
+#include <boost/variant.hpp>
+#include <boost/core/ignore_unused.hpp>
+#include <boost/exception/diagnostic_information.hpp>
+
+
+enum class Number
+{
+     One, Two, Three
+};
+
+
+std::ostream& operator<<( std::ostream& os, const Number n )
+{
+     switch( n )
+     {
+          case Number::One:
+               os << "One";
+               break;
+          case Number::Two:
+               os << "Two";
+               break;
+          case Number::Three:
+               os << "Three";
+               break;
+     }
+     return os;
+}
+
+
+struct Data
+{
+     Data( const std::string& s ) : number{ s } {}
+     Data( unsigned n ) : number{ n } {}
+     Data( Number n ) : number{ n } {}
+
+     boost::variant< std::string, unsigned, Number > number;
+};
+
+
+std::ostream& operator<<( std::ostream& os, const Data& d )
+{
+     os << d.number;
+     return os;
+}
+
+
+int main( int argc, char** argv )
+{
+     boost::ignore_unused( argc, argv );
+     try
+     {
+          std::cout << Data{ 50 } << '\n';
+          std::cout << Data{ "1011" } << '\n';
+          std::cout << Data{ Number::Three } << '\n';
+     }
+     catch( const std::exception& e )
+     {
+          std::cerr << "exception: " << boost::diagnostic_information( e ) << '\n';
+          return 1;
+     }
+     return 0;
+}
