edc623276f65aa7846f115f2c39a5829a9f778a5
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-11-23 16:08:03 +0300

    Log: Добавлен инициализатор для вывода в файл с ротацией по размеру

--- log/main.cpp
+++ log/main.cpp
@@ -15,16 +15,42 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
+#include <boost/utility/string_view.hpp>
+
+
+static std::size_t operator ""_KB( long long unsigned kb )
+{
+     return kb * 1024;
+}
 
 
 namespace show_higher_than_info {
+
 
 void init()
 {
      boost::log::core::get()->set_filter( boost::log::trivial::severity >= boost::log::trivial::info );
 }
 
+
 } // namespace show_higher_than_info
+namespace log_to_file_with_rotation {
+
+
+void init( boost::string_view logMask, std::size_t maxBytes = 10_KB )
+{
+     boost::log::add_file_log(
+          boost::log::keywords::file_name = logMask.to_string(),
+          boost::log::keywords::rotation_size = maxBytes,
+          boost::log::keywords::format = "[%TimeStamp%]: %Message%"
+          );
+     /// Эта запись нужна, чтобы работал плейсхолдер %TimeStamp%
+     boost::log::add_common_attributes();
+}
+
+
+} // namespace log_to_file_with_rotation
+
 
 
 
@@ -33,10 +59,13 @@ int main( int argc, char** argv )
      boost::ignore_unused( argc, argv );
      try
      {
-          show_higher_than_info::init();
+          log_to_file_with_rotation::init( "boost_log_trivial.%N", 1_KB );
 
-          BOOST_LOG_TRIVIAL( debug ) << "This is debug";
-          BOOST_LOG_TRIVIAL( info ) << "This is info";
+          for( auto i = 0u; i < 1_KB; ++i )
+          {
+               BOOST_LOG_TRIVIAL( debug ) << "This is debug";
+               BOOST_LOG_TRIVIAL( info ) << "This is info";
+          }
           BOOST_THROW_EXCEPTION( std::runtime_error{ "some kind of failure" } );
      }
      catch( const std::exception& e )
