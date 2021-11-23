5bb7a2b30dedc16f7b3fffbada8cf1583fc74271
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-11-23 15:44:59 +0300

    Log: Добавлена простейшая работа с логгированием

--- log/main.cpp
+++ log/main.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/core/ignore_unused.hpp>
 #include <boost/exception/diagnostic_information.hpp>
+#include <boost/log/trivial.hpp>
 
 
 int main( int argc, char** argv )
@@ -14,11 +15,13 @@ int main( int argc, char** argv )
      boost::ignore_unused( argc, argv );
      try
      {
-
+          BOOST_LOG_TRIVIAL( debug ) << "This is debug";
+          BOOST_LOG_TRIVIAL( info ) << "This is info";
+          BOOST_THROW_EXCEPTION( std::runtime_error{ "some kind of failure" } );
      }
      catch( const std::exception& e )
      {
-          std::cerr << "exception: " << boost::diagnostic_information( e ) << '\n';
+          BOOST_LOG_TRIVIAL( error ) << "exception: " << boost::diagnostic_information( e );
           return 1;
      }
      return 0;
