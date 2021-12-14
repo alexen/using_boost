03be9a59ef64067839d5bfc049a2eabbf0590238
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-12-14 23:14:48 +0300

    Log: Из main.cpp удален ненужный (и не работающий) код

--- log/main.cpp
+++ log/main.cpp
@@ -11,60 +11,12 @@
 #include <log/init.h>
 #include <log/handlers.h>
 
-using namespace using_boost::log;
-
-enum Severity
-{
-     Verbose,
-     Usual,
-     Suspicious,
-     Bad,
-     Deadly
-};
-
-
-std::ostream& operator<<( std::ostream& os, const Severity sev )
-{
-     switch( sev )
-     {
-          case Verbose:
-              os << "~verbose~";
-              break;
-          case Usual:
-              os << "~usual~";
-              break;
-          case Suspicious:
-              os << "~suspicious~";
-              break;
-          case Bad:
-              os << "~bad~";
-              break;
-          case Deadly:
-              os << "~deadly~";
-              break;
-          default:
-               os.setstate( std::ios::failbit );
-     }
-     return os;
-}
-
 
 int main( int argc, char** argv )
 {
      boost::ignore_unused( argc, argv );
      try
      {
-#if 0
-          using_boost::log::init::limitSeverity( boost::log::trivial::info );
-          using_boost::log::init::setLogRotation( "boost_log_trivial.%N", 1_KB );
-          using_boost::log::handler::starter::run();
-#endif
-          boost::log::sources::severity_logger< Severity > log;
-
-          BOOST_LOG_SEV( log, Usual ) << "This is Usual";
-          BOOST_LOG_SEV( log, Verbose ) << "This is Verbose";
-          BOOST_LOG_SEV( log, Deadly ) << "This is Deadly";
-          BOOST_LOG_SEV( log, Bad ) << "This is Bad";
      }
      catch( const std::exception& e )
      {
