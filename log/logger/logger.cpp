a7dceb7ff4ab2659cb3a51037c72ffea15534946
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2021-12-26 16:30:57 +0300

    Log: Добавлена вывод метки времени для приемника std::ostream&

--- log/logger/logger.cpp
+++ log/logger/logger.cpp
@@ -5,18 +5,20 @@
 
 #include <unistd.h>
 
+#include <boost/log/attributes.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/sinks/syslog_backend.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/attributes/current_thread_id.hpp>
 #include <boost/log/sinks/syslog_constants.hpp>
 #include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/support/date_time.hpp>
 
 #include <boost/core/null_deleter.hpp>
 #include <boost/make_shared.hpp>
 
 
+BOOST_LOG_ATTRIBUTE_KEYWORD( TimeStamp, "TimeStamp", boost::log::attributes::local_clock::value_type )
 BOOST_LOG_ATTRIBUTE_KEYWORD( ThreadId, "ThreadID", boost::log::attributes::current_thread_id::value_type )
 BOOST_LOG_ATTRIBUTE_KEYWORD( FilePath, "File", boost::string_view )
 BOOST_LOG_ATTRIBUTE_KEYWORD( FileLine, "Line", unsigned )
@@ -53,9 +55,9 @@ void syslog()
           boost::log::sinks::syslog::direct_severity_mapping< int >( "Severity" )
           );
      auto sink = boost::make_shared< Sink >( syslogBackend );
+     boost::log::add_common_attributes();
      sink->set_formatter( formatter );
      boost::log::core::get()->add_sink( sink );
-     boost::log::add_common_attributes();
 }
 
 
@@ -64,19 +66,21 @@ void ostream( std::ostream& os )
      using Backend = boost::log::sinks::text_ostream_backend;
      using Sink = boost::log::sinks::synchronous_sink< Backend >;
 
-     auto sink = boost::make_shared< Sink >();
-     sink->locked_backend()->add_stream( boost::shared_ptr< std::ostream >{ &os, boost::null_deleter{} } );
-
-     boost::log::add_common_attributes();
-     sink->set_formatter(
+     boost::log::formatter formatter =
           boost::log::expressions::stream
-               << '{' << getpid()
+               << '[' << TimeStamp
+               << "] {" << getpid()
                << '.' << ThreadId
                << "} (" << FilePath
                << ':' << FileLine
                << ") <" << boost::log::trivial::severity
-               << "> " << boost::log::expressions::message
-          );
+               << "> " << boost::log::expressions::message;
+
+     auto sink = boost::make_shared< Sink >();
+     sink->locked_backend()->add_stream( boost::shared_ptr< std::ostream >{ &os, boost::null_deleter{} } );
+
+     boost::log::add_common_attributes();
+     sink->set_formatter( formatter );
 
      boost::log::core::get()->add_sink( sink );
 }
