159c3b6ab1166e2631ec0b124920735eb1e165fb
alexen <alexen0000@yandex.ru>
2022-11-24 16:01:35 +0300

    Exception: Реализована методика объединения типов через boost::tuple

--- exception/main.cpp
+++ exception/main.cpp
@@ -5,10 +5,25 @@
 #include <stdexcept>
 #include <iostream>
 
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/string_generator.hpp>
 #include <boost/core/ignore_unused.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 
 #include "tools.h"
+#include "uuid_exception.h"
+
+
+void func()
+{
+     using namespace std::string_literals;
+
+     static const auto uuid = boost::uuids::string_generator{}( "84de4630-8fa0-4631-a905-87a6467a08d1" );
+     BOOST_THROW_EXCEPTION( alexen::error::Exception{}
+          << alexen::error::details::ErrorUuid( uuid )
+          << alexen::error::details::ErrorDescribedUuid( "just random UUID"s, uuid )
+          );
+}
 
 
 int main( int argc, char **argv )
@@ -16,7 +31,7 @@ int main( int argc, char **argv )
      boost::ignore_unused( argc, argv );
      try
      {
-          alexen::tools::doSmth( alexen::tools::Enum::Field2 );
+          func();
      }
      catch( const std::exception &e )
      {
---
+++ exception/uuid_exception.h
@@ -0,0 +1,24 @@
+/// @file uuid_exception.h
+/// @brief
+/// @copyright Copyright 2022 InfoTeCS Internet Trust
+
+#pragma once
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/exception/info_tuple.hpp>
+
+#include "user_exception.h"
+
+
+namespace alexen {
+namespace error {
+namespace details {
+
+
+using ErrorUuid = boost::error_info< struct ErrorUuid_, boost::uuids::uuid >;
+using ErrorDescribedUuid = boost::tuple< ErrorDescription, ErrorUuid >;
+
+
+} // namespace details
+} // namespace error
+} // namespace alexen
