74e132343fb2ad85bf7d4cf310b7e108d53c351d
Enakaev Aleksey <enakaev.aleksey@yandex.ru>
2023-05-04 10:50:50 +0300

    MakeSfx: Параметры скрипта приведены к единообразию

--- cmake/MakeSfx.cmake
+++ cmake/MakeSfx.cmake
@@ -5,10 +5,19 @@ if(NOT MAKESELF_PROGRAM)
      message(FATAL_ERROR "'makeself' utility not found, please install it")
 endif()
 
-set(SFX_INSTALLER_PREFIX "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_SYSTEM_NAME}")
-set(SFX_INSTALLER_NAME "${SFX_INSTALLER_PREFIX}.sh")
+if(NOT SFX_INSTALLER_NAME)
+     set(SFX_INSTALLER_NAME "${PROJECT_NAME}-${PROJECT_VERSION}-${CMAKE_SYSTEM_NAME}.sh")
+endif()
 
-set(TEMP_INSTALL_DIR /tmp/_${CMAKE_INSTALL_PREFIX})
+if(NOT SFX_INSTALLER_DESCRIPTION)
+     set(SFX_INSTALLER_DESCRIPTION "${PROJECT_NAME}: version ${PROJECT_VERSION} (${CMAKE_SYSTEM_NAME})")
+endif()
+
+if(SFX_INSTALLER_LICENSE_FILE)
+     set(MAKESELF_PARAM_ADD_LICENSE "--license ${SFX_INSTALLER_LICENSE_FILE}")
+endif()
+
+set(TEMP_INSTALL_DIR ${CMAKE_BINARY_DIR}/_sfxtmp)
 add_custom_command(
      OUTPUT ${SFX_INSTALLER_NAME}
      COMMAND ${CMAKE_COMMAND}
@@ -20,10 +29,11 @@ add_custom_command(
                --keep-umask
                --needroot
                --target ${CMAKE_INSTALL_PREFIX}
-               --license ${CPACK_RESOURCE_FILE_LICENSE}
+               ${MAKESELF_PARAM_ADD_LICENSE}
                ${TEMP_INSTALL_DIR}
                ${SFX_INSTALLER_NAME}
-               ${SFX_INSTALLER_PREFIX}
+               ${SFX_INSTALLER_DESCRIPTION}
      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+     VERBATIM
 )
 add_custom_target(sfx DEPENDS ${SFX_INSTALLER_NAME})
