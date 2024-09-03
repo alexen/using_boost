0206337383514180252b1061fbf64e83af3b750a
alexen <enakaev.aleksey@yandex.ru>
2024-09-03 12:30:27 +0300

    Beast: Отправка POST вынесена в отдельный тест, добавлен urlResolver

--- src/beast/first.cpp
+++ src/beast/first.cpp
@@ -32,66 +32,90 @@ namespace http = boost::beast::http;
 //User-Agent: Apache-HttpClient/4.1.1 (java 1.5)
 
 
+void main_simplePost( int, char** )
+{
+     static constexpr auto host = "docs-test.fss.ru";
+     static constexpr auto protocol = "https";
+     static constexpr auto path = "/ws-insurer-v11/FileOperationsLnPort";
+     static constexpr auto httpVersion = 10u;
+
+     static const boost::filesystem::path soapBodyPath = "./soap_body.xml";
+     static constexpr auto soapAction =
+          "http://ru/ibs/fss/ln/ws/FileOperationsLn.wsdl/getPrivateLNData";
+     static constexpr auto contentType = "text/xml;charset=UTF-8";
+
+     boost::asio::io_context ioctx;
+
+     boost::asio::ssl::context sslctx{ boost::asio::ssl::context::sslv23_client };
+
+     tcp::resolver resolver{ ioctx };
+     boost::asio::ssl::stream< tcp::socket > stream{ ioctx, sslctx };
+
+     boost::asio::connect( stream.next_layer(), resolver.resolve( host, protocol ) );
+
+     stream.handshake( boost::asio::ssl::stream_base::client );
+
+     boost::system::error_code error;
+     http::file_body::value_type body;
+     body.open( soapBodyPath.c_str(), boost::beast::file_mode::read, error );
+     if( error )
+     {
+          BOOST_THROW_EXCEPTION( boost::system::system_error{ error } );
+     }
+
+     http::request< http::file_body > request{ http::verb::post, path, httpVersion };
+     request.set( http::field::host, host );
+     request.set( http::field::user_agent, __PRETTY_FUNCTION__ );
+     request.set( http::field::content_length, std::to_string( body.size() ) );
+     request.set( http::field::soapaction, soapAction );
+     request.set( http::field::content_type, contentType );
+     request.body() = std::move( body );
+
+     http::write( stream, request );
+
+     boost::beast::flat_buffer buffer;
+     http::response< http::string_body > response;
+
+     http::read( stream, buffer, response );
+
+     if( response.result() == boost::beast::http::status::ok )
+     {
+          if( response.body().empty() )
+          {
+               BOOST_THROW_EXCEPTION( std::runtime_error{ "empty response received" } );
+          }
+          std::cout << response.body();
+     }
+
+     stream.shutdown();
+}
+
+
+void main_urlResolver( int, char** argv )
+{
+     std::cout << "IP addresses: \n";
+     boost::asio::io_service io_service;
+     boost::asio::ip::tcp::resolver resolver( io_service );
+     boost::asio::ip::tcp::resolver::query query( argv[ 1 ], "" );
+     for(
+          boost::asio::ip::tcp::resolver::iterator i = resolver.resolve( query );
+          i != boost::asio::ip::tcp::resolver::iterator();
+          ++i
+     )
+     {
+          boost::asio::ip::tcp::endpoint end = *i;
+          std::cout << end.address() << ' ';
+     }
+     std::cout << '\n';
+}
+
+
 int main( int argc, char** argv )
 {
      boost::ignore_unused( argc, argv );
      try
      {
-          static constexpr auto host = "docs-test.fss.ru";
-          static constexpr auto protocol = "https";
-          static constexpr auto path = "/ws-insurer-v11/FileOperationsLnPort";
-          static constexpr auto httpVersion = 10u;
-
-          static const boost::filesystem::path soapBodyPath =
-               argc > 1 ? argv[ 1 ] : "./soap_body.xml";
-          static constexpr auto soapAction =
-               "http://ru/ibs/fss/ln/ws/FileOperationsLn.wsdl/getPrivateLNData";
-          static constexpr auto contentType = "text/xml;charset=UTF-8";
-
-          boost::asio::io_context ioctx;
-
-          boost::asio::ssl::context sslctx{ boost::asio::ssl::context::sslv23_client };
-
-          tcp::resolver resolver{ ioctx };
-          boost::asio::ssl::stream< tcp::socket > stream{ ioctx, sslctx };
-
-          boost::asio::connect( stream.next_layer(), resolver.resolve( host, protocol ) );
-
-          stream.handshake( boost::asio::ssl::stream_base::client );
-
-          boost::system::error_code error;
-          http::file_body::value_type body;
-          body.open( soapBodyPath.c_str(), boost::beast::file_mode::read, error );
-          if( error )
-          {
-               BOOST_THROW_EXCEPTION( boost::system::system_error{ error } );
-          }
-
-          http::request< http::file_body > request{ http::verb::post, path, httpVersion };
-          request.set( http::field::host, host );
-          request.set( http::field::user_agent, __PRETTY_FUNCTION__ );
-          request.set( http::field::content_length, std::to_string( body.size() ) );
-          request.set( http::field::soapaction, soapAction );
-          request.set( http::field::content_type, contentType );
-          request.body() = std::move( body );
-
-          http::write( stream, request );
-
-          boost::beast::flat_buffer buffer;
-          http::response< http::string_body > response;
-
-          http::read( stream, buffer, response );
-
-          if( response.result() == boost::beast::http::status::ok )
-          {
-               if( response.body().empty() )
-               {
-                    BOOST_THROW_EXCEPTION( std::runtime_error{ "empty response received" } );
-               }
-               std::cout << response.body();
-          }
-
-          stream.shutdown();
+          main_urlResolver( argc, argv );
      }
      catch( const std::exception& e )
      {
